8056a4709710e03e92b63df6299b1a6f65842697
Degubi <gergonoorbi@gmail.com>
2023-10-21 07:11:37 +0200

    Add web build to release.yml

--- desktop/src/main/java/pdftableextractordesktop/Main.java
+++ desktop/src/main/java/pdftableextractordesktop/Main.java
@@ -19,7 +19,7 @@ import java.util.function.*;
 import javax.swing.*;
 
 public final class Main {
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
     public static final Jsonb json = JsonbBuilder.create(new JsonbConfig().withFormatting(Boolean.TRUE));
 
     public static void main(String[] args) throws Exception {
